7ab1a447fc5be8d9421d1f71601fe6ff8f061c95
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-05-12 00:59:57 -0300

    Incluindo mudanca de matiz e alteracao de texto do botao de tema com js
    Utilizando js para alterar o texto do botao tema e implementando matiz nas variaveis de cor

--- Trabalho-Final-Frontend-Cooperativa-Amazonas/script.js
+++ Trabalho-Final-Frontend-Cooperativa-Amazonas/script.js
@@ -36,5 +36,16 @@ function fetchApiData() {
 
 function mudaTema() {
     document.body.classList.toggle("dark");
-    
-  }
+    mudaTextoBotaoTema();
+
+}
+
+function mudaTextoBotaoTema() {
+    var x = document.getElementById("button-tema");
+    if (x.innerHTML === "Dark Mode") {
+        x.innerHTML = "Light Mode";
+    } else {
+        x.innerHTML = "Dark Mode";
+        // alert('cuidado com os bugs atraídos pela luz')
+    }
+}
